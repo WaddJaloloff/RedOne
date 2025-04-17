5945ea52cb8addd77ed1c822d8f73880bb7ce1db
Huzayfaa <fazliddindjalolov77@gmail.com>
2025-04-17 19:39:48 +0500

    3ta pagega backenddan datala jonatildi

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('products/', products, name='products'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -6,3 +6,11 @@ from .models import *
 def index(request):
     products = Product.objects.all() #.filter(category__category='wax')
     return render(request, 'home.html', {'products': products})
+
+def products(request):
+    products = Product.objects.all() 
+    return render(request, 'products.html', {'products': products})
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'product-detail.html', {'product': product})
