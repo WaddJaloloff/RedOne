0ce9d5743ce8c187172d6fad42bed1e4cd85fcbb
Huzayfaa <fazliddindjalolov77@gmail.com>
2025-04-22 15:59:19 +0500

    yengili

--- app/admin.py
+++ app/admin.py
@@ -3,14 +3,14 @@ from .models import Product, Categories
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'about']
+    list_display = ['id', 'name', 'price', 'about']
     search_fields = ['name', 'price', 'about']
 
 admin.site.register(Product, ProductAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['category']
+    list_display = ['id', 'category']
     search_fields = ['category']
 
 admin.site.register(Categories, CategoryAdmin)
