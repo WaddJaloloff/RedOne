55988a0cf6ba7b640727b36d5b4dd7d13295ae89
Huzayfaa <fazliddindjalolov77@gmail.com>
2025-04-28 18:30:53 +0500

    yo

--- app/models.py
+++ app/models.py
@@ -34,6 +34,31 @@ class Product(models.Model):
             return f"{self.price2:,}".replace(",", " ")
         return ""
     
+class Top_product(models.Model):
+    name_uz = models.CharField(max_length=200)
+    name_ru = models.CharField(max_length=200)
+    size1 = models.CharField(max_length=10)
+    price1 = models.IntegerField()
+    size2 = models.CharField(max_length=10, null=True, blank=True)
+    price2 = models.IntegerField(null=True, blank=True)
+    about_uz = models.TextField(null=True, blank=True)
+    about_ru = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images')
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name_uz
+    
+    @property
+    def formatted_price1(self):
+        return f"{self.price1:,}".replace(",", " ")
+
+    @property
+    def formatted_price2(self):
+        if self.price2:
+            return f"{self.price2:,}".replace(",", " ")
+        return ""
+    
 # def save(self, *args, **kwargs):
 #             if len(self.name_uz) > 20:
 #                 self.name_uz = self.name_uz[:20].rsplit(' ', 1)[0]
--- app/views.py
+++ app/views.py
@@ -6,15 +6,14 @@ from .models import Product, Categories
 
 # Create your views here.
 def index(request):
-    products = Product.objects.filter(is_active=True).select_related('category')[:4]
-    categories = Categories.objects.filter(product__is_active=True).distinct()
-    
+    last_products = Product.objects.filter(is_active=True).select_related('category')[-4:]
+    top_products = Top_product.objects.filter(is_active=True)[:4]
     # Maxsus kesilgan nomlarni olish
     for product in products:
         product.name_uz = product.name_uz[:20].rsplit(' ', 1)[0] if len(product.name_uz) > 20 else product.name_uz
         product.name_ru = product.name_ru[:20].rsplit(' ', 1)[0] if len(product.name_ru) > 20 else product.name_ru
 
-    return render(request, 'home.html', {'products': products, 'categories': categories})
+    return render(request, 'home.html', {'last_products': last_products, 'top_products': top_products})
 
 
 def products(request):
