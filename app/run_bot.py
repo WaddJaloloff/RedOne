4fb2322afc3384cc4ca71a3728e10d077681aac1
Huzayfaa <fazliddindjalolov77@gmail.com>
2025-05-02 18:11:55 +0500

    t

--- app/run_bot.py
+++ app/run_bot.py
@@ -6,14 +6,19 @@ bot = telebot.TeleBot("7186731988:AAGj2kEO-taQ-ACoC6JLzaVHxDDpVdbjVWU")
 def send_order_to_group(phone_number, order_items):
     chat_id = -4757061565
     order = f"📞 *Buyurtmachi raqami:* `{phone_number}`\n\n📦 *Buyurtma tafsilotlari:*\n"
-    
+    print("order_item", order_items)
     for item in order_items:
+        print(item)
         name = item['name']
         quantity = item['quantity']
         price = item['price']
+        size = item['size']
         total = quantity * price
-        order += f"▪️ *{name}*\n   └ {quantity} × {price:,} = {total:,} so'm\n"
-
+        order += f"▪️ *{name}*\n   └ {quantity} × {price:,} = {total:,} so'm"
+        if size is not None:
+            print("size:", size)
+            order+= f"({size})"
+        order+="\n"
     total_sum = sum(item['quantity'] * item['price'] for item in order_items)
     order += f"\n💰 *Umumiy summa:* {total_sum:,} so'm"
 
